29a5af8d03599deac5aa60ca0bf3d19a932c8d1b
nishiura yosuke <kenkenpa221202@gmail.com>
2023-05-31 15:56:37 +0900

    サンプル完成

--- ma.py
+++ ma.py
@@ -7,7 +7,7 @@ import webbrowser
 
 st.title('申請書')
 
-#st.write('DataFrame')
+st.write('DataFrame')
 
 #df = pd.DataFrame(
  #   np.random.rand(100,2)/[50,50]+[35.69,139.70],
@@ -35,15 +35,19 @@ st.title('申請書')
  #   st.write('Goodbye')
 
 def open_link():
-    webbrowser.open_new("<https://www.google.com>")
+    webbrowser.open_new(
+        "<https://logoform.jp/f/T64Xk/1145073?key=3af8caf1f2bb0014b0c9b046d1e376f7b8065170617264317f71b981444e9be9>"
+        )
 
 
 if st.button("youtube申請"):
     open_link()
 
 def open_link2():
-    webbrowser.open_new("<https://www.apple.com>")
+    webbrowser.open_new(
+        "<https://logoform.jp/f/Mc4rD/1159399?key=203cdbdb060f1e9dd3b1519b609a321b13285f51692664ea66c393391bb0b47a>"
+        )
 
 
-if st.button("個人ファイル保存申請"):
+if st.button("リーダー研修会Ⅰ"):
     open_link2()
