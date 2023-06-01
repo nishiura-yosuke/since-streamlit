8bc70e479c0022f6afbb7b65ff10a07d7bd23bf4
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-01 14:00:14 +0900

    123

--- ma.py
+++ ma.py
@@ -4,8 +4,6 @@ from PIL import Image
 
 st.title('申請書')
 
-# ボタンの作成
-if st.button('ボタンを押してください'):
-  image = Image.open('you.png')
-  st.image(image, caption='画像のキャプション')
+image = Image.open('you.png')
+st.image(image, caption='画像のキャプション')
 
