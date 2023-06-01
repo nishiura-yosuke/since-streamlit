b3c1dd9dcaecd991dd4b59132897b7eb4b94c7e7
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-01 13:58:30 +0900

    iou

--- ma.py
+++ ma.py
@@ -4,17 +4,8 @@ from PIL import Image
 
 st.title('申請書')
 
-
-image = Image.open('you.png')
-
-# ボタンが押されたら実行される関数
-def display_text_and_image():
-    st.write(
-        "<https://logoform.jp/f/Mc4rD/1159399?key=203cdbdb060f1e9dd3b1519b609a321b13285f51692664ea66c393391bb0b47a>"
-    
-             )
-    st.image(image, caption='画像のキャプション')
-
 # ボタンの作成
 if st.button('ボタンを押してください'):
-    display_text_and_image()
+  image = Image.open('you.png')
+  st.image(image, caption='画像のキャプション')
+
