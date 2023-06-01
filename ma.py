28ef895f1aca8bae44e1ee4e123ab6545f14a5f5
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-01 14:15:24 +0900

    rt

--- ma.py
+++ ma.py
@@ -6,7 +6,9 @@ image = Image.open('you.png')
 
 # ボタンが押されたら実行される関数
 def display_text_and_image():
-    st.write("テキストが表示されます。")
+    st.write(
+        "<https://logoform.jp/f/T64Xk/1145073?key=3af8caf1f2bb0014b0c9b046d1e376f7b8065170617264317f71b981444e9be9>"
+    )
     st.image(image, caption='画像のキャプション')
 
 # ボタンの作成
