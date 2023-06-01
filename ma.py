971ca54080e1bfecc1c233f7967c74ef58da894f
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-01 14:20:53 +0900

    200

--- ma.py
+++ ma.py
@@ -9,7 +9,7 @@ def display_text_and_image():
     st.write(
         "<https://logoform.jp/f/T64Xk/1145073?key=3af8caf1f2bb0014b0c9b046d1e376f7b8065170617264317f71b981444e9be9>"
     )
-    st.image(image, caption='youtube動画申請',width=100)
+    st.image(image, caption='youtube動画申請',width=200)
 
 # ボタンの作成
 if st.button('youtube動画申請'):
