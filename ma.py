21beeaea914c8a17eb0d1cb26a62e5b595cbbbc1
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-01 14:32:10 +0900

    b2

--- ma.py
+++ ma.py
@@ -17,3 +17,16 @@ def display_text_and_image():
 # ボタンの作成
 if st.button('youtube動画申請'):
     display_text_and_image()
+    
+    image2 = Image.open('you.png')
+
+# ボタンが押されたら実行される関数
+def display_text_and_image2():
+    st.write(
+        "<https://logoform.jp/f/T64Xk/1145073?key=3af8caf1f2bb0014b0c9b046d1e376f7b8065170617264317f71b981444e9be9>"
+    )
+    st.image(image2, caption='youtube動画申請',width=200)
+
+# ボタンの作成２
+if st.button('リーダー研修会Ⅰ'):
+    display_text_and_image2()
