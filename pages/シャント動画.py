d88c2bfb513672e18a0949dfab17ad81b585ea2a
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-05 09:22:12 +0900

    pagesおｋ

---
+++ pages/シャント動画.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('シャント動画')
+
+if st.button('動画1'):
+
+   st.video('https://youtu.be/Wu6Q79qiZpI', format="video/mp4", start_time=0)
+   
+if st.button('動画2'):
+
+   st.video('https://youtu.be/Wu6Q79qiZpI', format="video/mp4", start_time=0)
+   
+if st.button('動画3'):
+
+   st.video('https://youtu.be/Wu6Q79qiZpI', format="video/mp4", start_time=0)
