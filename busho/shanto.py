560fa03ddc5ac775eb3c853264465f247ef1d8c4
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-08 10:33:10 +0900

    sha

---
+++ busho/shanto.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title('シャント動画')
+
+if st.button('動画1'):
+
+   st.video('https://youtube.com/shorts/9d24QSoT5Es?feature=share', format="video/mp4", start_time=0)
+   
+if st.button('動画2'):
+
+   st.video('https://youtu.be/XbQ06MScArk', format="video/mp4", start_time=0)
+   
+if st.button('動画3'):
+
+   st.video('https://youtu.be/RYcr48VCNjs', format="video/mp4", start_time=0)
+
+if st.button('動画4'):
+
+   st.video('https://youtube.com/shorts/tWUzvQIvmAw?feature=share', format="video/mp4", start_time=0)
+   
+if st.button('動画5'):
+
+   st.video('https://youtube.com/shorts/lPnSd6GgW3A?feature=share', format="video/mp4", start_time=0)
